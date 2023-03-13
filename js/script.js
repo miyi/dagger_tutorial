c176cd077a4c3fb709868f8f40ee56a24fd31ffb
miyi <mylegojesus@gmail.com>
2023-03-13 08:58:16 -0700

    with dagger

--- js/script.js
+++ js/script.js
@@ -16,15 +16,34 @@
 // </code>
 // `;
 
+const highlight = () => {
+  // hljs.highlightAll();
+  document.querySelectorAll("code").forEach(function (element) {
+    debugger;
+    element.innerHTML = element.innerHTML
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+  });
+  hljs.highlightAll();
+};
+
+export const render = (code) => {
+  setTimeout(highlight, 100);
+  return code;
+};
+
 const testCode = `
 <code class="html"><meta author="Joe Example"></code>
 `;
 
 const testCode2 = `
 <div>hello world<div>
-`
+`;
 
-const loading = () => ({
+export const loading = () => ({
   code: testCode,
-  code2: testCode2
+  code2: testCode2,
 });
