895647ec7840b3f85c773a92804f7ae79b715205
miyi <mylegojesus@gmail.com>
2023-03-17 11:08:37 -0700

    work around with raw

--- js/script.js
+++ js/script.js
@@ -1,12 +1,44 @@
-const codeBlockA1 = `
-<div class="content">
-<div class="card">
-  <div class="title">title</div>
-  <div class="assignee">assigned to:</div>
-  <div class="info">content</div>
-</div>
+const cardXML = `
+<div class="card" +loading="{
+  title: 'Create Card',
+  assignee: 'me',
+  info: 'Play around with the +loading directive in dagger.js.'
+}">
+  <div class="title">\${title}</div>
+  <div class="assignee">assigned to: \$\{assignee}</div>
+  <div class="info">\$\{info}</div>
 </div>
 `;
+
+function isDagger(str) {
+  return (
+    str.startsWith("+") ||
+    str.startsWith("$") ||
+    str.startsWith("dg") ||
+    str.startsWith("@")
+  );
+}
+
+function removeAttributes(htmlString) {
+  // create a temporary element to hold the html
+  var tempEl = document.createElement("div");
+  tempEl.innerHTML = htmlString;
+
+  // loop through all elements and remove their attributes
+  var elements = tempEl.getElementsByTagName("*");
+  for (var i = 0; i < elements.length; i++) {
+    var attrs = elements[i].attributes;
+    for (var j = attrs.length - 1; j >= 0; j--) {
+      isDagger(attrs[j].name)
+        ? true
+        : elements[i].removeAttribute(attrs[j].name);
+    }
+  }
+
+  // return the modified html as a string
+
+  return tempEl.innerHTML;
+}
 
 // const highlight = (element) => {
 //   // hljs.highlightAll();
@@ -20,16 +52,14 @@ const codeBlockA1 = `
 //   hljs.highlightElement(element);
 // };
 
-export const renderCode = (code) => hljs.highlight(code, {language: 'xml'}).value
-
-const testCode = `
-<meta author="Joe Example">
-`;
-
-const testCode2 = `
-<div>hello world<div>
-`;
+export const renderCode = (code) => {
+  return `<div @raw>${hljs.highlight(code, { language: "xml" }).value}</div>`;
+};
 
 export const loading = () => ({
-  code: codeBlockA1,
+  rawHTML: cardXML,
+  showcaseHTML: removeAttributes(cardXML),
+  title: "create card",
+  assignee: "John Doe",
+  info: "create a card with dagger",
 });
